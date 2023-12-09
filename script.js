0a2358fecba133f26680ef73de1c7afd96362b16
ABlam87 <adamblampied@gmail.com>
2023-12-09 11:11:44 +0000

    added color code functionality

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-text area for planner middle
-save button adds to local Storage
+// text area for planner middle
+// save button adds to local Storage
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+today = $('#currentDay');
+today.text(dayjs().format('dddd, MMMM Do'));
+
+// to do
+
+// add color code functionality to text-area
+
+const nowHour = dayjs().format('HH');
+
+$('.textarea').each(function() {
+    const textarea = $(this);
+    
+    const plannerHour = textarea.attr('data-hour');
+    
+    if (nowHour < plannerHour) {
+        textarea.addClass('future');
+    } else if (nowHour == plannerHour) {
+        textarea.addClass('present');
+    } else {
+        textarea.addClass('past');
+    }
+});
+
+//save button commits text-area text to local storage
+
